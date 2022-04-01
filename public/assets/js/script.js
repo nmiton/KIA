9310a9edf11e4ea1c27cce69444d559967540b24
TheGeogeo <trolface87@hotmail.fr>
2022-04-01 17:24:49 +0200

    sound pas fini parce que vas-y le progre c'est pas forcement bien

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    const chien    = document.getElementById('chien');  
+    const chien = document.getElementById('chien');
     const block_chien = document.getElementById('img_animal')
-    const chien_queue =  document.getElementById('queue'); 
+    const chien_queue = document.getElementById('queue');
 
     let monAnimal = new Object();
     monAnimal.caresses = 0;
@@ -10,35 +10,35 @@ document.addEventListener("DOMContentLoaded", function() {
     //~ On lance l'initialisation 
     init();
     //~ Déclenchement de base
-    function init(){
+    function init() {
         //~ On assigne un évènement à chaque bouton : tu cliques ou tu meures
-        chien.addEventListener('click', function(){
+        chien.addEventListener('click', function () {
             //~ Go go go le calcul \o/
             setCaresse()
             screamer()
         });
     }
 
-    function setCaresse(){
+    function setCaresse() {
         monAnimal.caresses++;
-        if(monAnimal.caresses>10){
+        if (monAnimal.caresses > 10) {
             monAnimal.caresses = 0
         }
         setEstContent()
     }
 
-    function setEstContent(){
-        if(monAnimal.caresses > 3){
+    function setEstContent() {
+        if (monAnimal.caresses > 3) {
             monAnimal.estContent = true
-        }else{
+        } else {
             monAnimal.estContent = false
         }
     }
 
-    function screamer(){
-        if(monAnimal.caresses>5 & monAnimal.estContent) {
+    function screamer() {
+        if (monAnimal.caresses > 5 & monAnimal.estContent) {
             chien.style.position = 'inherit'
-            setInterval("chien.style.position = 'relative'",800)
+            setInterval("chien.style.position = 'relative'", 800)
         }
     }
 
