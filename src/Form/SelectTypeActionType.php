78cc477b5d977f5f95c57309161b3496cc140ba0
Nathan <nathan.miton@orange.fr>
2022-04-12 16:18:07 +0200

    Maj Access User

--- src/Form/SelectTypeActionType.php
+++ src/Form/SelectTypeActionType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class SelectTypeAction extends AbstractType
+class SelectTypeActionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
